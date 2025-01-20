b539ac2277378bb0e6221d44ce9f845a5464a395
KurtisL7 <kurtislin24@gmail.com>
2025-01-20 14:59:10 -0500

    adding Navbar

--- FrontEnd/pokerbot/src/App.js
+++ FrontEnd/pokerbot/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Navbar from './components/Navbar';
+
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 
@@ -9,7 +10,7 @@ function App() {
     <>
     <Router>
       <Navbar />
-      
+    
     </Router>
      </>
   );
